98ceecbc69734e9b9c14a2182770c17c409e3ea7
Salmontree <brexiosaurus@gmail.com>
2026-07-27 17:12:20 -0400

    remove all indents, just replace them w prefix

--- src/main/java/com/ottertree/nevada/util/ChatUtil.java
+++ src/main/java/com/ottertree/nevada/util/ChatUtil.java
@@ -9,7 +9,7 @@ public class ChatUtil {
     private static final Minecraft mc = Minecraft.getMinecraft();
     
     public static final String PREFIX = "§r§8[§5" + Nevada.NAME + "§8] §r";
-    public static final String INDENT = "§r  §5▌ §r";
+    public static final String INDENT = "§r§8[§5" + Nevada.NAME + "§8] §r"; // "§r  §5▌ §r";
 
     public static void send(String text) {
         mc.thePlayer.addChatMessage(new ChatComponentText(text));
